f8d37c7abaede52aa1c9776d5ad45fc7e4345674
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-06-02 12:42:38 -0400

    server work

--- Develop/server/schemas/revolvers.js
+++ Develop/server/schemas/revolvers.js
@@ -1,17 +1,30 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async () => {
-        const userData = await User.findOne(
-            { _id: user._id });
-            return userData;
-          },
-          throw : Authentication ('you are not logged in')
-        }
-    };
-   
+      const userData = await User.findOne({ _id: user._id });
+      return userData;
+    },
+    throw: Authentication("you are not logged in"),
+  },
+};
+//   Mutation: {
+//     createMatchup: async (parent, args) => {
+//       const matchup = await Matchup.create(args);
+//       return matchup;
+//     },
+//     createVote: async (parent, { _id, techNum }) => {
+//       const vote = await Matchup.findOneAndUpdate(
+//         { _id },
+//         { $inc: { [`tech${techNum}_votes`]: 1 } },
+//         { new: true }
+//       );
+//       return vote;
+//     },
+//   },
+// };
 
 module.exports = resolvers;
